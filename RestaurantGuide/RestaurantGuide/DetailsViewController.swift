ae3a9ce6750b0abbe53a511f2d8c89137179163a
CalvinPierce <77972370+CalvinPierce@users.noreply.github.com>
2022-03-13 16:23:10 -0400

    Initial Commit

---
+++ RestaurantGuide/RestaurantGuide/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  RestaurantGuide
+//
+//  Created by Tech on 2022-03-13.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
